a7ba4b5223046615c8161034f60e23e2e4ac3e64
Tristan/Libmonkey <8639535+libmonkey@users.noreply.github.com>
2021-04-17 17:58:39 +0200

    Implement giphy_random_api helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,20 @@
 module HomeHelper
+
+  def HomeHelper.giphy_random_api(number)
+    giphys = Array.new
+    url = URI("http://api.giphy.com/v1/gifs/random")
+
+    Net::HTTP.start(url.host, url.port) do |http|
+      request = Net::HTTP::Get.new(url)
+      request["api_key"] = Rails.application.credentials.giphy_api_key
+
+      number.times {
+        response = http.request(request)
+
+        giphys << JSON.parse(response.body)
+      }
+    end
+
+    giphys
+  end
 end
