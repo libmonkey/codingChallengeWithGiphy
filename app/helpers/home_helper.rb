27e6f60a0daf193ba1986f4e23047eed4b473c16
Tristan/Libmonkey <8639535+libmonkey@users.noreply.github.com>
2021-04-25 19:30:31 +0200

    Modify api result

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,29 +2,45 @@ module HomeHelper
 
   def HomeHelper.giphy_random_api()
     giphys = Array.new
-    url = URI("http://api.giphy.com/v1/gifs/random")
-    request = Net::HTTP::Get.new(url)
+    uri = URI("http://api.giphy.com/v1/gifs/random")
+    params = {api_key: Rails.application.credentials.giphy_api_key}
+    uri.query = URI.encode_www_form(params)
 
-    self.api_request(url, request, 20)
+    response = self.api_request(uri, 20)
+
+    giphys = Array.new
+
+    response.each do |giphy|
+      giphys << giphy["data"]
+    end
+
+    return giphys
   end
 
   def HomeHelper.giphy_search(search)
     giphys = Array.new
-    url = URI("http://api.giphy.com/v1/gifs/search")
-    request = Net::HTTP::Get.new(url)
-    request['q'] = "hallo"
-    request['limit'] = 20
-    debugger
-    self.api_request(url, request)
+    uri = URI("http://api.giphy.com/v1/gifs/search")
+    params = {api_key: Rails.application.credentials.giphy_api_key, q: "hallo" , limit: 20}
+    uri.query = URI.encode_www_form(params)
+
+    response = self.api_request(uri)
+
+    giphys = Array.new
+
+    response.first["data"].each do |giphy|
+      giphys << giphy
+    end
+
+    return giphys
   end
 
   private
 
-  def self.api_request(url, request, number_of_requests = 1)
+  def self.api_request(uri, number_of_requests = 1)
     giphys = Array.new
 
-    Net::HTTP.start(url.host, url.port) do |http|
-      request["api_key"] = Rails.application.credentials.giphy_api_key
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Get.new(uri)
 
       number_of_requests.times do
         response = http.request(request)
